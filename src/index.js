476ecadb0939f7f88541ee7bad7c23512dee127e
aaronlrv <aaronvitharanage@gmail.com>
2022-06-29 16:18:15 +0400

    Add event listeners to nav buttons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import firstModule from "../src/firstmodule"
-import contact from "../src/contact"
-import menu from "../src/menu"
+import firstModule from "./firstmodule"
+import contact from "./contact"
+import menu from "./menu"
 
-menu.generateMenu()
 
+let menuBtn = document.querySelector(".menuButton")
+let homeBtn = document.querySelector(".homeBtn")
+let contactBtn = document.querySelector(".contactBtn")
+
+menuBtn.addEventListener("click", () => {
+    console.log("Menu Button Working!")
+
+})
+
+homeBtn.addEventListener("click", () => {
+    console.log("Home Button Working!")
+})
+
+contactBtn.addEventListener("click", () => {
+    console.log("Contact Button Working!")
+})
 
 console.log("Hello World!")
+
+
