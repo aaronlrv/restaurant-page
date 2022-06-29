90272256426245fd035f2d1a8351bcea3b6af638
aaronlrv <aaronvitharanage@gmail.com>
2022-06-29 16:45:42 +0400

    Fix bug that generate tabs without clearing

--- src/index.js
+++ src/index.js
@@ -6,20 +6,28 @@ import menu from "./menu"
 let menuBtn = document.querySelector(".menuButton")
 let homeBtn = document.querySelector(".homeBtn")
 let contactBtn = document.querySelector(".contactBtn")
+let container = document.querySelector("#content")
+
+/* lets user initially start on homepage*/
+firstModule.first()
+
 
 menuBtn.addEventListener("click", () => {
     console.log("Menu Button Working!")
+    container.textContent = ""
     menu.generateMenu()
 
 })
 
 homeBtn.addEventListener("click", () => {
     console.log("Home Button Working!")
+    container.textContent = ""
     firstModule.first()
 })
 
 contactBtn.addEventListener("click", () => {
     console.log("Contact Button Working!")
+    container.textContent = ""
     contact.contactInfo()
 })
 
